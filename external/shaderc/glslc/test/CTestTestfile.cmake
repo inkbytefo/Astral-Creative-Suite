b4cd95cf5ee08d27c16485a4e61ce31f080882e8
inkbytefo <inkbytefo@gmail.com>
2025-09-11 18:14:42 +0300

    chore: Harici kütüphaneleri izlemeyi bırak

--- external/shaderc/glslc/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/tpoyr/OneDrive/Desktop/AstralEngine/external/shaderc/glslc/test
-# Build directory: C:/Users/tpoyr/OneDrive/Desktop/AstralEngine/external/shaderc/glslc/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- external/shaderc/libshaderc_util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/tpoyr/OneDrive/Desktop/AstralEngine/external/shaderc/libshaderc_util
-# Build directory: C:/Users/tpoyr/OneDrive/Desktop/AstralEngine/external/shaderc/libshaderc_util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
